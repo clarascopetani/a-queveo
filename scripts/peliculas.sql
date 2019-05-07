d324f916ddb46776e7c11493e5f7c1256852b5fa
clarascop <clarascopetani@gmail.com>
2019-05-07 09:32:08 -0300

    Version Base V0.0

---
+++ scripts/peliculas.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE queveo;
+
+USE queveo;
+
+CREATE TABLE `pelicula` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `duracion` int(5) NOT NULL,
+  `director` varchar(400) NOT NULL,
+  `anio` int(5) NOT NULL,
+  `fecha_lanzamiento` date NOT NULL,
+  `puntuacion` int(2) NOT NULL,
+  `poster` varchar(300) NOT NULL,
+  `trama` varchar(700) NOT NULL,  
+  PRIMARY KEY (`id`)
+);
